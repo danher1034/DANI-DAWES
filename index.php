e44eda4d56969d12ea15739183178f4dba12c2ac
danher1034 <danaguher6@alu.edu.gva.es>
2023-09-25 16:57:36 +0200

    primer commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+  /**
+   * Archivo index juego de cartas
+   * @author Dani Agullo Heredia
+   * @version 1.0
+   */
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>página principa</title>
+</head>
+
+<body>
+  <h1>
+    Página principal
+  </h1>
+  
+  <?php
+    require_once(__DIR__.'/required/header.php')
+  ?> <!--Este php se encarga de colocar la cabecera en todas las páginas  -->
+
+</body>
+
+</html>
---
+++ required/header.php
@@ -0,0 +1,9 @@
+<?php
+  /**
+   * Archivo index juego de cartas
+   * @author Dani Agullo Heredia
+   * @version 1.0
+   */
+
+   
+?>
