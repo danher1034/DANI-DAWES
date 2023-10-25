9deb977de4941066cda0c0c51d9ffa9b291a62c3
danher1034 <danaguher6@alu.edu.gva.es>
2023-10-25 18:40:03 +0200

    revels

--- Nueva carpeta/index.php
+++ Nueva carpeta/index.php
@@ -4,6 +4,9 @@
  * @author Dani Agullo Heredia
  * @version 1.0
  */
+
+require_once(__DIR__.'/includes/User.inc.php');
+require_once(__DIR__.'/includes/regularExpression.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,20 +21,43 @@
 </head>
 
 <body class="body-index">
-    <nav class="navbar" data-bs-theme="dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">
-                <img src="img/logo-revels.png" alt="Logo" width="55" height="50">
-            </a>
-            <h1>REVELS</h1>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
-    <aside class="sidebar">SIDEBAR</aside>
-    <article class="main">MAIN</article>
+    <div class="Sign-up" >
+        <h2>REVELS</h2><br>
+        <h4>Regístrate para ver revels de tus amigos</h4>
+        <?php
+                echo '<form action="#" method="post" enctype="multipart/form-data">';           
+                        if (isset($_POST['user'])) {
+                            if (count($errors) < 1) {
+                                header('Location: /index.php');
+                                exit;
+                            }
+                        }
+                        echo '<br> Mail o Teléfono: <input type="text" name="mail|phone"><br>'; // Los siguiente if se encargan de crear los input para cada apartado
+                        if (isset($errors['mail|phone'])) {
+                            echo '<br>' . $errors['mail|phone'] . '<br>';
+                        }
+                        echo '<br> Nombre: <input type="text" name="name"><br>';
+                        if (isset($errors['name'])) {
+                            echo '<br>' . $errors['name'] . '<br>';
+                        }
+                        echo '<br>Usuario: <input type="text" name="user"><br>';  
+                        if (isset($errors['user'])) {
+                            echo '<br>' . $errors['user'] . '<br>';
+                        }
+                        echo '<br> Contraseña : <input type="text" name="password"><br>';
+                        if (isset($errors['password'])) {
+                            echo '<br>' . $errors['password'] . '<br>';
+                        }
+                        echo '<br>';
+                        echo '<br>';
+                        echo '<input type="submit" value="Enviar">';     
+                echo '</form>';
+        
+        ?>
+    </div>
+    <div class="Sign-in" >
+         <p>¿Tienes cuenta en revels? <a href="login.php">Inicia sesión</a></p>
+    </div>
     <footer class="footer">FOOTER</footer>
 </body>
 
--- Nueva carpeta/login.php
+++ Nueva carpeta/login.php
@@ -4,29 +4,11 @@
  * @version 1.0
  */
 require_once(__DIR__.'/includes/User.inc.php');
+require_once(__DIR__.'/includes/regularExpression.php');
 
-$mailAnduserExpression ='/^([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$)|[A-z]{3,20}/';
-$passwordExpression ='/^((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\S+$)(?=.*[;:\.,!¡\?¿@#\$%\^&\-_+=\(\)\[\]\{\}])).{8,20}$/';
- 
-if (isset($_POST['mail|user'])) { //comprueba si el campo existe
-    if (strlen($_POST['mail|user'] > 0)) {
-        if (!preg_match($mailAnduserExpression, $_POST['mail|user'])) { //comprueba que el correo introducido sea valido
-            $errors['mail|user'] = 'El mail deber serguir el siguiente ***@***.*** ';
-        }
-    } else {
-        $errors['mail|user'] = 'No se ha introducido ningun dato en el mail ni usuario';
-    }
-}
 
-if (isset($_POST['password'])) { //comprueba si el campo existe
-    if (strlen($_POST['password'] > 0)) {
-        if (!preg_match($passwordExpression, $_POST['password'])) { //comprueba que la fecha introducida sea valida
-            $errors['password'] = 'La contraseña debe contener al menos 1 letra mayuscula, un caracter especial ;:\.,!¡\?¿@#\$%\^&\-_+= y un digito';
-        }
-    } else {
-        $errors['password'] = 'No se ha introducido ninguna dato en la contraseña';
-    }
-}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -38,32 +20,32 @@ if (isset($_POST['password'])) { //comprueba si el campo existe
     <title>Login</title>
 </head>
 <body class="body-login">
-<div class="Sign-in" >
-    <h2>REVELS</h2>
-    <h4>Regístrate para ver revels de tus amigos.</h4>
-    <form action="#" method="post" enctype="multipart/form-data">
-        <?php
-            if (isset($_POST['mail|user'])) {
-                if (count($errors) < 1) {
-                    header('Location: /index.php');
-                    exit;
+    <div class="Sign-in" >
+        <h2>REVELS</h2><br>
+        <h4>Inicia sesión para ver revels de tus amigos.</h4>
+        <form action="#" method="post" enctype="multipart/form-data">
+            <?php
+                if (isset($_POST['mail|user'])) {
+                    if (count($errors) < 1) {
+                        header('Location: /index.php');
+                        exit;
+                    }
                 }
-            }
-            echo '<br> Mail o Usuario: <input type="text" name="mail|user"><br>'; // Los siguiente if se encargan de crear los input para cada apartado
-            if (isset($errors['mail|user'])) {
-                echo '<br>' . $errors['mail|user'] . '<br>';
-            }
-            echo '<br> Contraseña : <input type="text" name="password"><br>';
-            if (isset($errors['password'])) {
-                echo '<br>' . $errors['password'] . '<br>';
-            }
-            echo '<br>';
-            echo '<br>';
-            echo '<input type="submit" value="Enviar">';
-        ?>
-</div>
+                echo '<br> Mail o Usuario: <input type="text" name="mail|user"><br>'; // Los siguiente if se encargan de crear los input para cada apartado
+                if (isset($errors['mail|user'])) {
+                    echo '<br>' . $errors['mail|user'] . '<br>';
+                }
+                echo '<br> Contraseña : <input type="text" name="password"><br>';
+                if (isset($errors['password'])) {
+                    echo '<br>' . $errors['password'] . '<br>';
+                }
+                echo '<br>';
+                echo '<br>';
+                echo '<input type="submit" value="Enviar">';
+            ?>
+    </div>
 <div class="Sign-up" >
-    <p>¿No tienes cuenta en revels? <a href="login - copia.php">Regístrate</a></p>
+    <p>¿No tienes cuenta en revels? <a href="index.php">Regístrate</a></p>
 </div>
 <footer class="footer">FOOTER</footer>
 
