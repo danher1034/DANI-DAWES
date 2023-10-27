be5de737f7e8fbe25e6abd4c79d1ab46b5571156
danher1034 <danaguher6@alu.edu.gva.es>
2023-10-27 18:39:09 +0200

    u

---
+++ Nueva carpeta/user.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body class="body-user">
+    <nav class="navbar">
+            <a class="navbar-brand" href="index.php">
+                <img src="img/logo-revels.png" alt="Logo" width="55" height="50">
+            </a>
+            <h1>REVELS</h1>
+            <form class="nav_form">
+                <input type="text" placeholder="Buscar...">
+                <button><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+    </nav>
+    <aside class="sidebar">
+            <img src="Idyd_Roberto-Dupuis-600.png" alt="" height="30%" >
+            <br>
+            <br>
+            <h3>Manolo</h3>
+            <br>
+            <div class="followers">
+                <p class="revels">6 revels</p>
+                <p class="follower">166 seguidores</p>
+                <p class="follows">168 seguidos</p>
+            </div>
+            <p>Manolo El Manuelas <br>
+               Diseñador de los manolitos <br>
+               Manolo El crack</p>
+    </aside>
+    <article class="main">
+            main
+    </article>
+    <footer class="footer">FOOTER</footer>
+</body>
+</html>
