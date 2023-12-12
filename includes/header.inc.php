f82ff9797cb54251976645c1ab0040bfd9e53a5d
danher1034 <danaguher6@alu.edu.gva.es>
2023-12-12 14:24:16 +0100

    svV

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -8,4 +8,9 @@
 <header>
     <a href="/index.php?rick"><h2>Rick</h2></a>
     <a href="/index.php?morty"><h2>Morty</h2></a>  
+    <form action="#" class="nav_form" method="post" class="coment_form" enctype="multipart/form-data">
+        <label for="users"></label> 
+        <input class="input-nav" name="users" name="users" type="text" placeholder="Buscar..." required>
+        <button class="button-nav" type="submit" id="comment"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </form>
 </header>
--- index.php
+++ index.php
@@ -13,28 +13,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rick & Morty</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.cdnfonts.com/css/healing-lighters" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <?php
-        if(isset($_GET['rick'])){
-            $endpoint='https://rickandmortyapi.com/api/character/?name=rick';
-            $data=file_get_contents($endpoint);
-            $data=json_decode($data);
-        }elseif(isset($_GET['morty'])){
-            $endpoint='https://rickandmortyapi.com/api/character/?name=morty';
-            $data=file_get_contents($endpoint);
-            $data=json_decode($data);
-             
+        if(isset($_GET['rick'])){ //si recibe $_GET['rick']
+            $endpoint='https://rickandmortyapi.com/api/character/?name=rick'; //creo Endpoint con la consulta a la api y sus resultados
+            $data=file_get_contents($endpoint); //guardamos en data los resultados de la api
+            $data=json_decode($data); //decodificamos el json
+        }elseif(isset($_GET['morty'])){//si recibe $_GET['morty']
+            $endpoint='https://rickandmortyapi.com/api/character/?name=morty';//creo Endpoint con la consulta a la api y sus resultados
+            $data=file_get_contents($endpoint);//guardamos en data los resultados de la api
+            $data=json_decode($data); //decodificamos el json 
         }
 
-        if(isset($data)){
-            foreach($data->$results as $name_value){
-                $characters[
-                    'name'=$name_value->$name
-                    'image'=$name_value->$name
-                ]           
+        if (isset($data->results)) {// Recorrer $data todos los resultados
+            foreach ($data->results as $character) {
+                if (isset($character->name, $character->image)) { // Verificar si las claves 'name' e 'image' existen en el objeto actual                   
+                    $characters[] = array( // Agregar el nombre y la imagen al array 'characters'
+                        'name' => $character->name,
+                        'image' => $character->image,
+                    );
+                }
             }
         }
+
+        
+        while (isset($data->info->next) && $data->info->next !== null) { // Iniciar un bucle que se repetirá mientras exista una próxima página
+            // Obtener la siguiente página
+            $endpoint = $data->info->next;
+            $data = file_get_contents($endpoint);
+            $data = json_decode($data);
+
+            // Recorrer los resultados de la página actual y agregar al array 'characters'
+            foreach ($data->results as $character) {
+                if (isset($character->name, $character->image)) {
+                    $characters[] = array(
+                        'name' => $character->name,
+                        'image' => $character->image,
+                    );
+                }
+            }
+        }
+
+        echo '<div class="characters">';
+            foreach ($characters as $character) {
+                echo '<div class="characters">
+                <div class="character">
+                    <h3>'.$character['name'].'</h3>
+                    <img src="'.$character['image'].'" alt="'.$character['name'].'">
+                </div>';
+            }
+        echo ' </div>';
     ?>
+    
 </body>
 </html>
